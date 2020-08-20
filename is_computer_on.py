6e06b8f7c8d303d52e33d53f13f77466eb77ced0
Sandi Baressi Šegota <ssegota@gmail.com>
2020-08-20 08:07:59 +0200

    Added docstring

--- is_computer_on.py
+++ is_computer_on.py
@@ -1,9 +1,20 @@
-#Function for checking if the computer is turned on
-#Very useful
-#Takes no arguments
-#If the computer is on returns True
-#If the computer is not on, the behaviour is undefined
-#NOTE: Yes, this is a joke
+"""Function for checking if the computer is turned on
+Very useful
+Takes no arguments
+If the computer is on returns True
+If the computer is not on, the behaviour is undefined
+NOTE: Yes, this is a joke
+
+"""
 def is_computer_on():
+    """Summary or Description of the Function
+
+    Parameters:
+    None
+
+    Returns:
+    bool:Returning value
+    
+    """
     return True
 
